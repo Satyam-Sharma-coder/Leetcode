a121bbab4e700585aae0483ea53154955825211c
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-01-25 02:32:59 +0530

    Time: 5 ms (100%), Space: 59.7 MB (54.4%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,27 +1,21 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
         int n = nums.length;
-        int[] lastIndex = new int[10001]; // Tracks last seen index for each number
-        int i = 0; // Start of the sliding window
+        int[] lastIndex = new int[10001];
+        int i = 0;
         int maxSum = 0, currentSum = 0;
 
         for (int j = 0; j < n; j++) {
             int num = nums[j];
-
-            // If the number was seen before and is within the current window, shrink the window
             if (lastIndex[num] > 0) {
                 while (i < lastIndex[num]) {
-                    currentSum -= nums[i]; // Remove elements from the window sum
+                    currentSum -= nums[i];
                     i++;
                 }
             }
-
-            // Add the current number to the window
             currentSum += num;
             maxSum = Math.max(maxSum, currentSum);
-
-            // Update the last seen index of the current number
-            lastIndex[num] = j + 1; // Use j + 1 to differentiate unvisited elements (default 0)
+            lastIndex[num] = j+1;
         }
 
         return maxSum;
