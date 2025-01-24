9d1d7f7ff367a872c179d89bf93fbfea4296837c
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-01-24 22:46:34 +0530

    Time: 6 ms (18.18%), Space: 63.2 MB (44.15%) - LeetHub

---
+++ 3483-alternating-groups-ii/3483-alternating-groups-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numberOfAlternatingGroups(int[] colors, int k) {
+        int start = 0;
+        int end = 1;
+        int ans = 0;
+        int ans2=0;
+        while(end<2*colors.length){
+            if(colors[(end-1)% colors.length]==colors[(end)% colors.length]){
+                start = end;
+            }
+            else if(colors[(end-1)%colors.length]!=colors[end % colors.length] && end-start+1>=k){
+                ans++;
+            }
+            if(end== colors.length-1){
+                ans2=ans;
+            }
+            end++;
+        }
+        return ans-ans2;
+    }
+}
