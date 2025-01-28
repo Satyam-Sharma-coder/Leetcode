14e94f0840b4fd4f3d3a315df11ab13cb3f530fa
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-01-28 15:17:47 +0530

    Time: 26 ms (40.11%), Space: 79.4 MB (48.02%) - LeetHub

--- 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
+++ 1695-maximum-sum-obtained-of-any-permutation/1695-maximum-sum-obtained-of-any-permutation.java
@@ -1,55 +1,23 @@
-// class Solution {
-//     public int maxSumRangeQuery(int[] nums, int[][] requests) {
-//         int[] arr= new int[nums.length];
-//         for(int i=0;i<requests.length;i++){
-//             arr[requests[i][0]]++;
-//             if(requests[i][1]+1<arr.length){
-//                 arr[requests[i][1]+1] = -1;
-//             }
-//         }
-//         for(int i=1; i<arr.length;i++){
-//             arr[i] = arr[i]+arr[i-1];
-//         }
-//         Arrays.sort(arr);
-//         Arrays.sort(nums);
-//         long sum=0;
-//         for(int i=nums.length-1;i>=0;i--){
-//             sum += (long)arr[i]*nums[i]%1_000_000_007;
-//         }
-//         return (int)sum;
-//     }
-// }
-
-import java.util.Arrays;
-
 class Solution {
     public int maxSumRangeQuery(int[] nums, int[][] requests) {
-        int[] arr = new int[nums.length];
-        
-        // Marking frequency changes
-        for (int i = 0; i < requests.length; i++) {
+        int[] arr= new int[nums.length];
+        for(int i=0;i<requests.length;i++){
             arr[requests[i][0]]++;
-            if (requests[i][1] + 1 < arr.length) {
-                arr[requests[i][1] + 1]--;
+            if(requests[i][1]+1<arr.length){
+                arr[requests[i][1]+1]--;
             }
         }
-        
-        // Computing prefix sums
-        for (int i = 1; i < arr.length; i++) {
-            arr[i] += arr[i - 1];
+        for(int i=1; i<arr.length;i++){
+            arr[i] = arr[i]+arr[i-1];
         }
-        
-        // Sorting arrays
         Arrays.sort(arr);
         Arrays.sort(nums);
-        
-        // Calculating maximum sum
-        long sum = 0;
-        int mod = 1_000_000_007;
-        for (int i = nums.length - 1; i >= 0; i--) {
-            sum = (sum + (long)arr[i] * nums[i]) % mod;
+        long sum=0;
+        for(int i=nums.length-1;i>=0;i--){
+            sum = (sum + (long)nums[i]*arr[i])%1_000_000_007;
         }
-        
         return (int)sum;
     }
 }
+
+
