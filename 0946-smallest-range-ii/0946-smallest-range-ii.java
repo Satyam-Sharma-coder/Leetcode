5d75f875321d7edc71d0eafa1505aadc7937ac15
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-01-29 11:52:48 +0530

    Time: 11 ms (93.89%), Space: 45.2 MB (89.96%) - LeetHub

--- 0946-smallest-range-ii/0946-smallest-range-ii.java
+++ 0946-smallest-range-ii/0946-smallest-range-ii.java
@@ -3,12 +3,10 @@ class Solution {
         Arrays.sort(nums);
         int bestL = nums[0]+k;
         int bestR = nums[nums.length-1]-k;
-        int min=0;
-        int max= 0;
         int res=nums[nums.length-1]-nums[0];
         for(int i=0;i<nums.length-1;i++){
-            min=Math.min(bestL,nums[i+1]-k);
-            max=Math.max(bestR,nums[i]+k);
+            int min=Math.min(bestL,nums[i+1]-k);
+            int max=Math.max(bestR,nums[i]+k);
             res=Math.min(max-min,res);
         }
         return res;
