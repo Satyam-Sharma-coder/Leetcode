739674b8d6c54205e07411ebb407ffdc8e53dfd2
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-01-31 08:08:00 +0530

    Time: 4 ms (97.37%), Space: 45 MB (87.7%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -2,30 +2,31 @@ class Solution {
     class Pair{
         char ch;
         int count;
-
         Pair(char ch, int count){
-            this.ch = ch;
+            this.ch=ch;
             this.count=count;
         }
     }
     public String frequencySort(String s) {
         int[] arr = new int[125];
 
-        for(int i=0;i<s.length();i++){
+        for(int i=0; i<s.length();i++){
             arr[s.charAt(i)]++;
         }
-        List<Pair> freq=new ArrayList<>();
 
-        for(int i=48;i<=122;i++){
+        List<Pair> freq = new ArrayList<>();
+
+        for(int i=48;i<125;i++){
             if(arr[i]>0){
                 freq.add(new Pair((char)i,arr[i]));
             }
         }
-        Collections.sort(freq,new Comparator<Pair>() {
+        Collections.sort(freq, new Comparator<Pair>(){
             public int compare(Pair a, Pair b){
                 return b.count-a.count;
             }
         });
+
         StringBuilder sb = new StringBuilder();
 
         for(Pair p: freq){
@@ -35,7 +36,6 @@ class Solution {
                 count--;
             }
         }
-
         return sb.toString();
     }
 }
