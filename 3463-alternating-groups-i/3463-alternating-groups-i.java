c501108f4ea13cbb3db8b1a3494af32ec5eb93fc
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-02-05 15:53:56 +0530

    Time: 1 ms (94.81%), Space: 42.9 MB (93.77%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.java
+++ 3463-alternating-groups-i/3463-alternating-groups-i.java
@@ -1,22 +1,22 @@
 class Solution {
     public int numberOfAlternatingGroups(int[] colors) {
-        int start = 0;
-        int end = 1;
-        int ans = 0;
+        int start=0;
+        int end=1;
+        int k=3;
+        int ans1=0;
         int ans2=0;
-        int k = 3;
         while(end<2*colors.length){
-            if(colors[(end-1)% colors.length]==colors[(end)% colors.length]){
-                start = end;
+            if(colors[(end-1)%colors.length]==colors[(end)%colors.length]){
+                start=end;
             }
-            else if(colors[(end-1)%colors.length]!=colors[end % colors.length] && end-start+1>=k){
-                ans++;
+            else if(colors[(end-1)%colors.length]!=colors[(end)%colors.length] && end-start+1>=k){
+                ans1++;
             }
-            if(end== colors.length-1){
-                ans2=ans;
+            if(end==colors.length-1){
+                ans2=ans1;
             }
             end++;
         }
-        return ans-ans2;
+        return ans1-ans2;
     }
 }
