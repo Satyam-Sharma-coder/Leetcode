5dd23970d920d413c0eee40b31a8da7864657e49
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-02-05 16:50:17 +0530

    Time: 1 ms (52.21%), Space: 41.6 MB (58.39%) - LeetHub

---
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean areAlmostEqual(String s1, String s2) {
+        int count=0;
+        int[] arr1 = new int[26];
+        int[] arr2 = new int[26];
+        if(s1.length()!=s2.length()) return false;
+        for(int i=0;i<s1.length();i++){
+            arr1[s1.charAt(i)-'a']++;
+            arr2[s2.charAt(i)-'a']++;
+        }
+        for(int i=0;i<arr1.length;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        for(int i=0;i<s1.length();i++){
+            if(s1.charAt(i)!=s2.charAt(i)){
+                count++;
+            }
+            if(count>2){
+                return false;
+            }
+        }
+        return true;
+    }
+}
