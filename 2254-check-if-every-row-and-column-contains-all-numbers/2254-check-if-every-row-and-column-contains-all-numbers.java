b47f1f8cbf47f401d7ae1aeb2c8007049e532ed4
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-03-16 22:26:19 +0530

    Time: 26 ms (38.9%), Space: 46 MB (12.11%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -3,12 +3,11 @@ class Solution {
     for (int i = 0; i < matrix.length; ++i) {
       Set<Integer> set1 = new HashSet<>();
       Set<Integer> set2 = new HashSet<>();
-      for (int j = 0; j < matrix.length; ++j) {
+      for (int j = 0; j < matrix.length; j++) {
         set1.add(matrix[i][j]);
         set2.add(matrix[j][i]);
       }
-      if (Math.min(set1.size(), set2.size()) < matrix.length)
-        return false;
+      if (Math.min(set1.size(), set2.size()) < matrix.length) return false;
     }
     return true;
     }
