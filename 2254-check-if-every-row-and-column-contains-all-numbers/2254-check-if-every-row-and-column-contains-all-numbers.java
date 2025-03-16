4d663b620d7d6238620756adac092ff0526792ad
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-03-16 22:27:07 +0530

    Time: 25 ms (52.42%), Space: 44.9 MB (99.02%) - LeetHub

--- 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
+++ 2254-check-if-every-row-and-column-contains-all-numbers/2254-check-if-every-row-and-column-contains-all-numbers.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean checkValid(int[][] matrix) {
-    for (int i = 0; i < matrix.length; ++i) {
+    for (int i = 0; i < matrix.length; i++) {
       Set<Integer> set1 = new HashSet<>();
       Set<Integer> set2 = new HashSet<>();
       for (int j = 0; j < matrix.length; j++) {
