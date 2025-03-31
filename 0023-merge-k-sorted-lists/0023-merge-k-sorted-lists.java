5140741c65f55c915e410dbd2153e734db744bc9
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-03-31 22:04:37 +0530

    Time: 180 ms (5.04%), Space: 44.7 MB (38.4%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null || list2 == null){
+            return list1 == null? list2 : list1;
+        }
+        if(list1.val>list2.val){
+            ListNode temp = list1;
+            list1 = list2;
+            list2 = temp;
+        }
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        ListNode dummy = new ListNode(-10000); // Dummy node
+        ListNode mergedList = dummy.next; // Start merging from dummy.next
+        for (ListNode list : lists) {
+            mergedList = mergeTwoLists(mergedList, list); // Merge iteratively
+        }
+        return mergedList; // Return merged list
+
+    }
+}
+
+
+//  -10000--> 1  --> 4 --> 5
+//  1--> 3 --> 4
+
+
+
+// -10000 --> 1--> 1 --> 3 --> 4-->4-->5
+// 2-->6
