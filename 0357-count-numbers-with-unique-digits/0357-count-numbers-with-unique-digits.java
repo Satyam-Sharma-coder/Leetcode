c9103f8c7edc28456cb35e7b400790cb3825bc3b
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-04-30 09:14:58 +0530

    Time: 0 ms (100%), Space: 40.8 MB (13.14%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countNumbersWithUniqueDigits(int n) {
+        if(n==0) return 1;
+        int ans =10;
+        int start=9;
+        int curr=9;
+        while(n-->1){
+            curr *= start--;
+            ans += curr;
+        }
+        return ans;
+    }
+}
