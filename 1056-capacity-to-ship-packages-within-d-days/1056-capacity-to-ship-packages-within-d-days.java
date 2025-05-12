72d70c42a944f5e41e7b1f88c6ab570685181920
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-05-12 22:27:12 +0530

    Time: 11 ms (76.67%), Space: 47.3 MB (47.12%) - LeetHub

--- 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.java
@@ -1,35 +1,36 @@
 class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        if(days > weights.length) return -1;
-        int min = 0;
-        int max = 0;
-        for(int i: weights){
-            min = Math.max(min, i);
-            max += i;
+    public boolean isPossible(int[] nums,int k,int mid){
+        k--;
+        int sum=0;
+        for(int i:nums){
+            if(i > mid) return false;
+            if(sum+i>mid){
+                k--;
+                sum =0;
+            }
+            sum +=i;
         }
-        int ans = 0;
-        while(min <= max){
-            int mid = (min + max)/2;
-            if(isPossible(weights,mid,days)){
-                ans = mid;
-                max = mid - 1;
+        return k>=0;
+
+    }
+    public int shipWithinDays(int[] nums, int days) {
+        int min=Integer.MAX_VALUE;
+        int max=0;
+        for(int i:nums){
+            min = Math.min(min,i);
+            max+=i;
+        }
+        int ans=0;
+        while(min<=max){
+            int mid=(min+max)/2;
+            if(isPossible(nums,days,mid)){
+                ans=mid;
+                max=mid-1;
             }
             else{
-                min = mid + 1;
+                min=mid+1;
             }
         }
         return ans;
-    }
-    public static boolean isPossible(int[] weights, int mid, int days){
-        int capacity = 0;
-        days--;
-        for(int i: weights){
-            capacity += i;
-            if(capacity > mid){
-                days--;
-                capacity = i;
-            }
-        }
-        return days>=0;
     }
 }
