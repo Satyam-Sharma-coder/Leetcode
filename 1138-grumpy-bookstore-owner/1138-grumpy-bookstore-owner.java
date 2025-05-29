99a899b9809f199118dc0bc2e956139be5c76b39
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-05-30 01:36:35 +0530

    Time: 5 ms (19.26%), Space: 47.5 MB (62.76%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -1,25 +1,20 @@
 class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
-        int unsatisfied = 0;
-        for(int i=0;i<minutes;i++){
-            unsatisfied += customers[i] * grumpy[i];
-            
+        int s=0;
+        int e=0;
+        int us=0;
+        int mx=0;
+        int sf=0;
+        while(e<grumpy.length){
+            sf += grumpy[e]==0?customers[e]:0;
+            if(e-s+1>minutes){
+                us -= grumpy[s]*customers[s];
+                s++;
+            }
+            us += grumpy[e]*customers[e];
+            mx= Math.max(mx,us);
+            e++;
         }
-        int maxUnsatisfied = unsatisfied;
-        int start = 0;
-        int end = minutes;
-        while(end < grumpy.length){
-            unsatisfied = unsatisfied - (customers[start] * grumpy[start]) + (customers[end] * grumpy[end]);
-            maxUnsatisfied = Math.max(maxUnsatisfied,unsatisfied);
-            start++;end++;
-            
-        }
-        int satisfied=0;
-        for(int i=0;i<grumpy.length;i++){
-            if(grumpy[i]==0){
-                satisfied += customers[i];
-            }  
-        }
-        return maxUnsatisfied+satisfied;
+        return mx+sf;
     }
 }
