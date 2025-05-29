40ef932e8c1ab5a234ed7ceb847b395aa41bc8f8
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-05-30 01:40:29 +0530

    Time: 5 ms (19.26%), Space: 47.7 MB (39.68%) - LeetHub

--- 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
+++ 1138-grumpy-bookstore-owner/1138-grumpy-bookstore-owner.java
@@ -3,12 +3,12 @@ class Solution {
         int s=0,e=0,us=0,mx=0,sf=0;
         while(e<grumpy.length){
             sf += grumpy[e]==0?customers[e]:0;
-            if(e-s+1>minutes){
+            us += grumpy[e]*customers[e];
+            if(e-s+1==minutes){
+                mx= Math.max(mx,us);
                 us -= grumpy[s]*customers[s];
                 s++;
             }
-            us += grumpy[e]*customers[e];
-            mx= Math.max(mx,us);
             e++;
         }
         return mx+sf;
