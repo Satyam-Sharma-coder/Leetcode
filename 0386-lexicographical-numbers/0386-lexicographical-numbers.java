e998b1ddc20704fbe2af18717e1016fa5966a3f9
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-06-09 10:58:26 +0530

    Time: 13 ms (16.7%), Space: 58.5 MB (5.72%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.java
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -1,17 +1,24 @@
 class Solution {
     public List<Integer> lexicalOrder(int n) {
         ArrayList<Integer> list = new ArrayList<>();
-        answer(1,n,list);
+        HashSet<Integer> set = new HashSet<>();
+        answer(1,n,list,set);
         return list;
     }
-    public void answer(int i,int n,List<Integer> list){
-        if(i>n){
+    public void answer(int i,int n,List<Integer> list,HashSet<Integer> set){
+        if(set.contains(i) || i>n || i==0 ){
             return;
         }
         list.add(i);
-        answer(i*10,n,list);
-        if(i%10!=9){
-            answer(i+1,n,list);
+        set.add(i);
+        answer(i*10,n,list,set);
+        int last= i%10;
+        last=last+1;
+        i=i/10;
+        i=i*10+last;
+        while(i%10==0){
+            i=i/10;
         }
+        answer(i,n,list,set);
     }
 }
