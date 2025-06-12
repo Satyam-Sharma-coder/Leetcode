b6e4c9771e49f2b2a776e56875490a123af088a4
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-06-12 21:14:53 +0530

    Time: 32 ms (70.27%), Space: 46.1 MB (14.68%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.java
+++ 0876-hand-of-straights/0876-hand-of-straights.java
@@ -2,21 +2,18 @@ class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
 
         if(hand.length % groupSize!=0) return false;
+        
         Map<Integer,Integer> map = new HashMap<>();
         for(int i=0;i<hand.length;i++){
             map.put(hand[i],map.getOrDefault(hand[i],0)+1);
         }
-
-
         Arrays.sort(hand);
         for(int i=0;i<hand.length;i++){
             int first=hand[i];
             int j=1;
-            int k=-1;
             while(j!=groupSize && map.get(hand[i])!=0){
-                if(map.containsKey(hand[i]+k) && map.get(hand[i]+k)!=0){
+                if(map.containsKey(hand[i]+j) && map.get(hand[i]+j)!=0){
                     j++;
-                    k--;
                 }
                 else{
                     break;
@@ -24,11 +21,9 @@ class Solution {
             }
             if(j==groupSize){
                     j=0;
-                    k=0;
                     while(j!=groupSize){
-                        map.put(hand[i]+k,map.getOrDefault(hand[i]+k,0)-1);
+                        map.put(hand[i]+j,map.getOrDefault(hand[i]+j,0)-1);
                         j++;
-                        k--;
                     }
             }
         }
