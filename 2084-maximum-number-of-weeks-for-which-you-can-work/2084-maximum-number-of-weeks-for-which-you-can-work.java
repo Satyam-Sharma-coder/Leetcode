8c26b52bb0d32b75b0d1f7b60d1b73b63f6d801d
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-07-01 00:14:22 +0530

    Time: 23 ms (7.69%), Space: 56.1 MB (88.46%) - LeetHub

--- 2084-maximum-number-of-weeks-for-which-you-can-work/2084-maximum-number-of-weeks-for-which-you-can-work.java
+++ 2084-maximum-number-of-weeks-for-which-you-can-work/2084-maximum-number-of-weeks-for-which-you-can-work.java
@@ -3,14 +3,12 @@ class Solution {
         Arrays.sort(milestones);
         if (milestones.length==1) return milestones[0]==0?0:1;
         long sum=0;
-        for(int i=0;i<milestones.length;i++){
+        for(int i=0;i<milestones.length-1;i++){
             sum +=milestones[i];
         }
-        System.out.println(sum);
-        System.out.println(milestones[milestones.length-1]);
-        if(milestones[milestones.length-1]>sum-milestones[milestones.length-1]){
-            return 2*(sum-milestones[milestones.length-1])+1;
+        if(milestones[milestones.length-1]>sum){
+            return 2*(sum)+1;
         }
-        return sum;
+        return sum+milestones[milestones.length-1];
     }
 }
