2d205808ad2ea4a857eaa55a5510d58dca153255
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-07-03 15:03:38 +0530

    Time: 98 ms (85.49%), Space: 65.7 MB (92.31%) - LeetHub

--- 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
+++ 0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.java
@@ -18,18 +18,7 @@ class NumMatrix {
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        if(row1==0 && col1==0){
-            return matrix[row2][col2];
-        }
-        else if(row1==0 && col1!=0){
-            return matrix[row2][col2]-matrix[row2][col1-1];
-        }
-        else if(row1!=0 && col1==0){
-            return matrix[row2][col2]-matrix[row1-1][col2];
-        }
-        else{
-            return matrix[row2][col2]-matrix[row2][col1-1]-matrix[row1-1][col2]+matrix[row1-1][col1-1];
-        }
+        return row1==0?(col1==0? matrix[row2][col2] :matrix[row2][col2]-matrix[row2][col1-1]) : col1==0? matrix[row2][col2]-matrix[row1-1][col2] : matrix[row2][col2]-matrix[row2][col1-1]-matrix[row1-1][col2]+matrix[row1-1][col1-1];
     }
 }
 
