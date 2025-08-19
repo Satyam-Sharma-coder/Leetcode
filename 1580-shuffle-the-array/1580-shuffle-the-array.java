c2d36dfd8a6e27af8cabc1f891e6716412276f9b
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-08-19 16:59:56 +0530

    Time: 0 ms (100%), Space: 44.9 MB (26.08%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -1,16 +1,11 @@
 class Solution {
     public int[] shuffle(int[] nums, int n) {
-        int val = 1001;
-
+        int ans[]=new int[2*n];
+        int k=0;
         for(int i=0;i<n;i++){
-            nums[i+n]= nums[i+n] + (nums[i]*val);
+            ans[k++]=nums[i];
+            ans[k++]=nums[i+n];
         }
-
-        
-        for(int i=0;i<n;i++){
-            nums[2*i]=nums[i+n]/val;
-            nums[2*i+1]=nums[i+n]%val;
-        }
-        return nums;
+        return ans;
     }
 }
