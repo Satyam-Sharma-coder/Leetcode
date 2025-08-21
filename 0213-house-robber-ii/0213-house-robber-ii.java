6f53d65172bcbfe1b10be09436f70af8d3e3f55c
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-08-21 23:29:07 +0530

    Time: 0 ms (100%), Space: 41 MB (80.65%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,13 +1,26 @@
 class Solution {
+    public int helper(int i, int[] nums,int[] dp){
+        if(i<dp.length && dp[i]!=-1 ){
+            return dp[i];
+        }
+        if(i >= nums.length-1){
+            if(i==nums.length-1) return nums[i];
+            return 0;
+        }
+        int val1 = nums[i] + helper(i+2, nums,dp);
+        int val2 = nums[i+1] + helper(i+3, nums,dp);
+        return dp[i]=Math.max(val1, val2);
+    }
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-
-        int[] dp = new int[2*nums.length];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<dp.length;i++){
-            dp[i]=Math.max(dp[i-1],nums[i%nums.length]+dp[i-2]);
+        int[]dp=new int[nums.length];
+        int[]dp2=new int[2*nums.length];
+        Arrays.fill(dp,-1);
+        Arrays.fill(dp2,-1);
+        int[] nums2=new int[nums.length*2];
+        for(int i=0;i<2*nums.length;i++){
+            nums2[i]=nums[i%nums.length];
         }
-        return dp[2*nums.length-1]-dp[nums.length-1];
+        return helper(0,nums2,dp2)-helper(0,nums,dp);
     }
 }
