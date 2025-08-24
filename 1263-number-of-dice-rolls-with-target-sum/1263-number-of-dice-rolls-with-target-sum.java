495cb7c2759e4e06f9006e1ff51d34468fa69bf0
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-08-24 12:11:47 +0530

    Time: 9 ms (91.61%), Space: 41.9 MB (78.22%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.java
@@ -6,19 +6,19 @@ class Solution {
         if(n<=0 || target<=0){
             return 0;
         }
-        if(dp[n][target]!=-1){
-            return dp[n][target];
+        if(dp[n-1][target-1]!=-1){
+            return dp[n-1][target-1];
         }
         int ans=0;
         for(int i=1;i<=k;i++){
             ans=ans+helper(n-1,k,target-i,dp);
             ans=ans%1000_000_007;
         }
-        return dp[n][target]=ans;
+        return dp[n-1][target-1]=ans;
 
     }
     public int numRollsToTarget(int n, int k, int target) {
-        int[][] dp= new int[n+1][target+1];
+        int[][] dp= new int[n][target];
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
