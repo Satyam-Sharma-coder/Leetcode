cfc66ee9f25cad0ed0284812768e84ba21f2c61c
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-08-26 15:16:19 +0530

    Time: 0 ms (100%), Space: 40.9 MB (99.34%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.java
@@ -1,32 +1,38 @@
 class Solution {
-    HashSet<Integer> s = new HashSet<>();
-    public int mincostTickets(int[] days, int[] costs) {
-        int lastday = days[days.length-1]+30;
-        int[] dp =new int[lastday+1];
-        Arrays.fill(dp,-1);
-        for(int n: days){
-            s.add(n);
+    public int binarySearch(int[] days, int start,int end,int target){
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(days[mid]>=target){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
         }
-        return mincost(dp,days,costs, days[0]);
+        return start;
     }
-    public  int mincost(int [] dp,int[] days,int[] cost, int n){
-        if(n>days[days.length-1]){
-            dp[n]=0;
+    public int helper(int[] days,int[] costs,int i,int[] dp){
+        if(i>days.length-1){
             return 0;
         }
-        if(dp[n] != -1){
-            return dp[n];
-        }
-        if(!s.contains(n)){
-            dp[n] = mincost(dp,days,cost,n+1);
-            return  dp[n];
-        }
-        int oneday=cost[0]+ mincost(dp,days,cost,n+1);
-        int sevenday=cost[1]+ mincost(dp,days,cost,n+7);
-        int thirtyday=cost[2]+mincost(dp,days,cost,n+30);
-        dp[n]= Math.min(oneday, Math.min(sevenday,thirtyday));
-        return dp[n];
+        if(dp[i]!=-1) return dp[i];
 
 
+
+        int two=costs[0]+helper(days,costs,i+1,dp);
+
+        int dayv=days[i]+7;
+        int idx=binarySearch(days,i,days.length-1,dayv);
+        int seven=costs[1]+helper(days,costs,idx,dp);
+
+        int dayv2=days[i]+30;
+        int idx2=binarySearch(days,i,days.length-1,dayv2);
+        int thirty=costs[2]+helper(days,costs,idx2,dp);
+        return dp[i]=Math.min(two,Math.min(seven,thirty));
+    }
+    public int mincostTickets(int[] days, int[] costs) {
+        int[] dp= new int[days.length];
+        Arrays.fill(dp,-1);
+        return helper(days,costs,0,dp);
     }
 }
