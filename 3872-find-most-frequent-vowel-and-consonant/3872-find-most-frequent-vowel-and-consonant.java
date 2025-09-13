b24e740eac6bace5c914a5b09e7e8202aa82ed96
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-09-13 08:18:09 +0530

    Time: 1 ms (100%), Space: 42.3 MB (85.77%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.java
@@ -4,10 +4,11 @@ class Solution {
         for(int i=0;i<s.length();i++){
             freq[s.charAt(i)-'a']++;
         }
-        int maxv=Math.max(freq[0],Math.max(freq[4],Math.max(freq[8],Math.max(freq[14],freq[20]))));
+        int maxv=0;
         int maxc=0;
         for(int i=0;i<26;i++){
             if(i==0 || i==4 || i==8 || i==14 || i==20){
+                maxv=Math.max(maxv,freq[i]);
                 continue;
             }
             maxc=Math.max(maxc,freq[i]);
