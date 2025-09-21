47fbb7e5e09a61a2dca2f99dea293cc92aed6619
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-09-21 13:55:44 +0530

    Time: 3 ms (42.38%), Space: 44.9 MB (73.34%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    public void helper(List<Integer> temp, List<List<Integer>> ans,Map<Integer,Integer> map,int length){
+        if(length==temp.size()){
+            ans.add(new ArrayList<>(temp));
+        }
+        for (int nums: map.keySet()){
+            int count = map.get(nums);
+            if(count==0){
+                continue;
+            }
+            temp.add(nums);
+            map.put(nums,count-1);
+
+            helper(temp,ans,map,length);
+            temp.remove(temp.size()-1);
+            map.put(nums,count);
+        }
+    }
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i: nums){
+            map.put(i,map.getOrDefault(i,0)+1);
+        }
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(new ArrayList<>(),ans,map,nums.length);
+        return ans;
+    }
+}
