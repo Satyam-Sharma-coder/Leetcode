2bec924aca58c7d8d79bc091d15f2b401b524a1f
Satyam-Sharma-coder <138916953+Satyam-Sharma-coder@users.noreply.github.com>
2025-10-04 14:02:13 +0530

    Time: 4 ms (90.04%), Space: 45.2 MB (68.99%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,42 @@
 class Solution {
-    public boolean canFinish(int V, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();
-        
-        // declare arraylist of adjacent nodes
-        for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
+    public boolean dfs(int u,boolean[] visited,boolean[] explored, List<List<Integer>> adj){
+        if(visited[u]==true && explored[u]==false){
+            return false;
         }
-        
-        // indegree of nodes(incoming nodes)
-        int[] indgree = new int[V];
-        
-        //inset adjacent nodes 
-        for(int[] edge: edges){
-            int u = edge[0];
-            int v =edge[1];
-            adj.get(u).add(v);
-            indgree[v]++;             // update indegee val
-        }
-        
-        //declare queue for nodes having indegree 0
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<V;i++){
-            if(indgree[i]==0){
-                q.add(i);
+        visited[u]=true;
+
+        for(int v: adj.get(u)){
+            if(!explored[u] && !dfs(v,visited,explored,adj)){
+                return false;
             }
         }
-        
-        
-        //
-        ArrayList<Integer> ans = new ArrayList<>();
-        
-        while(!q.isEmpty()){
-            int node = q.poll();
-            ans.add(node);
-            for(int Adj: adj.get(node)){
-                indgree[Adj]--;
-                if(indgree[Adj]==0){
-                    q.add(Adj);
+        explored[u]=true;
+        return true;
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+
+        for(int[] arr: prerequisites){
+            int v=arr[0];
+            int u=arr[1];
+            adj.get(u).add(v);
+        }
+
+        boolean[] visited=new boolean[numCourses];
+        boolean[] explored=new boolean[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(!dfs(i,visited,explored,adj)){
+                    return false;
                 }
             }
         }
-        return ans.size()==V;
+        return true;
+
     }
 }
